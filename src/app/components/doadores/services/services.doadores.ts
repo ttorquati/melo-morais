aad2939fb8559a2671ec096208bd302322b917e0
ttorquati <ttorquati@gmail.com>
2021-08-15 18:14:32 -0300

    alterando visual de tabelas no mobile

--- src/app/components/doadores/services/services.doadores.ts
+++ src/app/components/doadores/services/services.doadores.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { URLSearchParams } from 'url';
+import { Observable } from 'rxjs';
 
 import { environment } from '../../../../environments/environment';
 import { NovoDoador } from '../../cadastro-doador/model/novo-doador';
